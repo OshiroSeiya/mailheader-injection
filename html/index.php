9c84356547893add607a5ff78c5894d2755a10e0
oshiro_seiya <oshiro_seiya@ca-adv.co.jp>
2018-07-13 20:45:33 +0900

    お問い合わせフォーム(雑)を用意した

--- html/index.php
+++ html/index.php
@@ -1,7 +1,35 @@
 <?php
-$to      = 'example@example.com';
-$subject = 'タイトル';
-$message = '本文';
-$headers = 'From: example@example.com' . "\r\n";
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+  $to      = empty($_POST['to']) ? 'example@example.com' : $_POST['to'];
+  $subject = empty($_POST['subject']) ? 'テストサブジェクト' : $_POST['subject'];
+  $message = empty($_POST['message']) ? 'テスト本文' : $_POST['message'];
+  $from = empty($_POST['from']) ? 'example@example.com' : $_POST['from'];
+  $headers = 'From: ' . $from . "\r\n";
+  mb_send_mail($to, $subject, $message, $headers);
 
-mb_send_mail($to, $subject, $message, $headers);
+  $url = "http://${_SERVER['HTTP_HOST']}";
+  header('Location: ' . $url);
+  exit;
+}
+?>
+<!DOCTYPE html>
+<html lang = "ja">
+<head>
+  <meta charset = "UFT-8">
+  <title>お問い合わせ</title>
+</head>
+<body>
+  <h1>お問い合わせの送信</h1>
+  <form method="post">
+    <label for="to">To:</label>
+    <input type = "text" id="to" name ="to"><br/>
+    <label for="subject">Subject:</label>
+    <input type = "text" id="subject" name ="subject"><br/>
+    <label for="message">Message:</label>
+    <input type = "text" id="message" name ="message"><br/>
+    <label for="from">From:</label>
+    <input type = "text" id="from" name ="from"><br/>
+    <input type = "submit" value ="送信">
+  </form>
+</body>
+</html>
